b5cdc9389900a95ca5edc9df14e6993b8404fb15
Richard <rclancing@gmail.com>
2023-02-11 07:42:01 -0800

    pycodestyle fixed

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-"""class FileStorage that serializes instances to a JSON file and deserializes JSON file to instances:
+"""class FileStorage that serializes instances to a
+JSON file and deserializes JSON file to instances:
 """
 
 import json
@@ -13,22 +14,23 @@ from models.amenity import Amenity
 from models.state import State
 import re
 
+
 class FileStorage:
-    #private class attributes
-    #json file path
+    # private class attributes
+    # json file path
     __file_path = "file.json"
-    #dictionary storage for all objects of a class instance by class id
+    # dictionary storage for all objects of a class instance by class id
     __objects = dict()
 
-    #function to return the dictionary storing class instances
+    # function to return the dictionary storing class instances
     def all(self):
         return self.__objects
 
-    #function to update the dictionary storage with new class instances
+    # function to update the dictionary storage with new class instances
     def new(self, obj):
         self.__objects[obj.__class__.__name__ + "." + obj.id] = obj
 
-    #funtion to serialize the dictionary storage to a JSON file
+    # funtion to serialize the dictionary storage to a JSON file
     def save(self):
         """
             serializes __objects to the JSON file
@@ -39,7 +41,7 @@ class FileStorage:
         with open(self.__file_path, 'w', encoding="utf-8") as filename:
             json.dump(new_dict, filename)
 
-    #function to deserialize dictionary storage from a JSON file
+    # function to deserialize dictionary storage from a JSON file
     def reload(self):
         try:
             with open(self.__file_path, "r", encoding="utf-8") as file:
@@ -50,5 +52,4 @@ class FileStorage:
 
         else:
             for key, val in old_dict.items():
-                
                 self.__objects[key] = eval(val["__class__"])(**val)
